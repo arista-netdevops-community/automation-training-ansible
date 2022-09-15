3bd2c54905d85d0c06ab2c4dce970b647519a6c1
Guillaume Vilar <guillaume.vilar@arista.com>
2022-09-15 18:23:10 +0100

    [DAY-1][LAB-1]: Give some more hints on the python code side

--- Day-1/Lab-1/example.py
+++ Day-1/Lab-1/example.py
@@ -16,7 +16,7 @@ DEVICE_IPS = ['192.168.0.10',
               '192.168.0.17'
               ]
 
-# USE YOUR ATD CREDENTIALS 
+# TODO: USE YOUR ATD CREDENTIALS
 USERNAME = ''
 PASSWORD = ''
 
@@ -25,15 +25,18 @@ if __name__ == '__main__':
                'method': 'runCmds',
                'params': {
                  'version': 1,
-                 # commands to run
-                 'cmds': []
+                 # TODO: Modify the list of commands to run on the switches
+                 'cmds': ["show clock"]
                },
                'id': '1'
               }
 
     pp = PrettyPrinter()
 
+    # TODO: Add/modify the below code to iterate through all the IPs in the list 'DEVICE_IPS'
     device = DEVICE_IPS[0]
     r = requests.post('https://{}:443/command-api'.format(device), json=payload, auth=(USERNAME, PASSWORD), verify=False)
     response = r.json()
     pp.pprint(response)
+    # TODO: Print the output in the desired fortmat
+    # print(f"Example of formating a string: {response['result'][0]}")
