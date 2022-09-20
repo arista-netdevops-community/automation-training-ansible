2008a4b0735a3caaf56aefe82e8368d0c1e0efeb
Tamas Plugor <tamas@arista.com>
2022-09-20 18:59:29 +0100

    answers: add alternative solution to Day-1 Lab-2 challenge

---
+++ Day-1/Lab-2/main-alt.py
@@ -0,0 +1,57 @@
+import requests
+from jinja2 import Environment, FileSystemLoader
+from pprint import PrettyPrinter
+
+# disable warnings about self-signed certs
+import urllib3
+urllib3.disable_warnings()
+
+DEVICE_IPS = ['192.168.0.10',
+              '192.168.0.11',
+              '192.168.0.12',
+              '192.168.0.13',
+              '192.168.0.14',
+              '192.168.0.15',
+              '192.168.0.16',
+              '192.168.0.17'
+              ]
+              
+# USE YOUR CREDENTIALS
+USERNAME = ''
+PASSWORD = ''
+
+if __name__ == '__main__':
+    payload = {'jsonrpc': '2.0',
+               'method': 'runCmds',
+               'params': {
+                 'version': 1,
+                 'cmds': ['show version',
+                          'show hostname',
+                          'show ip arp']
+               },
+               'id': '1'
+              }
+    device_outputs = {}
+
+    pp = PrettyPrinter()
+    env = Environment(loader=FileSystemLoader("."))
+    template = env.get_template("example.j2")
+
+    for device in DEVICE_IPS:
+        r = requests.post('https://{}:443/command-api'.format(device), json=payload, auth=(USERNAME, PASSWORD), verify=False)
+        response = r.json()
+        #pp.pprint(response)
+        serialNumber = response['result'][0]['serialNumber']
+        hostname = response['result'][1]['hostname']
+        # TODO: Store the ARP information in a variable
+        # arp_table = ...
+        arp_table = response['result'][2]
+        # Here, we add an entry for each device in the dictionnary 'device_outputs'
+        # TODO: Add the ARP information to the dictionnary 'device_outputs' so that info can be used in the jinja template
+        device_outputs[hostname] = {'serial': serialNumber}
+        device_outputs[hostname]['arp_table'] = arp_table
+
+        # figure out how to store the ARP address table data in a way that will make your template rendering easy
+    else:
+        #pp.pprint(device_outputs)
+        print(template.render(devices=device_outputs))
